c7582c393231dae78b2803cbd770bd151374f53e
Marina Delgado <75797804+Marinadelgado88@users.noreply.github.com>
2021-05-31 11:04:29 +0200

    make excercise of the day

--- JS/31-05/index.js
+++ JS/31-05/index.js
@@ -7,3 +7,36 @@
 // 1- mayor a menor 
 // 2- menor a mayor 
 // mostrar el array ordenado
+
+// Opcion 1 ( Simple)
+//var numero = [2,5,4,7,3,5,7];
+//ordenElegido = prompt ("¿Cómo lo quieres ordenar: de 1.mayor a menor o 2.de menor a mayor?")
+
+//if( ordenElegido===1){
+   // document.write(7,7,5,5,4,3,2)
+//}else{
+ //   document.write(2,3,4,5,5,7,7)
+//}
+
+//Opcion 2( sort y reverse )
+// var numero = [2,5,4,7,3,5,7];
+// let ordenElegido = prompt ("¿Cómo lo quieres ordenar: de 1.mayor a menor o 2.de menor a mayor?")
+
+// num1=1;
+// num2=2;
+
+// if(ordenElegido==num1){
+//   document.write(numero.sort());
+// }
+// else{
+//    document.write(numero.reverse());
+// } 
+
+
+// pasos ejercicio con XML:
+// generar el array desde el XML
+// AÑADIR un botón para cada orden:
+// 1 - ascendente por nombre de usuario
+// 2 - asecendente por clave de usuario
+// 3 - mostrar solamente el usuario solicitado
+//    (input - .slice .indexOf)
