6affcd346e1331ae8ead4895bb6261792989538c
Marina Delgado <75797804+Marinadelgado88@users.noreply.github.com>
2021-05-31 09:29:09 +0200

    create structure

---
+++ JS/31-05/index.js
@@ -0,0 +1,9 @@
+// EJERCICIOS CON ARRAYS
+
+// pasos: 
+// crear el Array 
+// genenar 7 elementos de forma aleatoria entre 1 y 7
+// pedir por pantalla el metodo de ordenamiento: 
+// 1- mayor a menor 
+// 2- menor a mayor 
+// mostrar el array ordenado
