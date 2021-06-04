2697ca4439884a26240935c0f1d70bf4205aebcd
Marina Delgado <75797804+Marinadelgado88@users.noreply.github.com>
2021-06-04 09:49:43 +0200

    change data

--- portfolio/Semana4/xml/app.js
+++ portfolio/Semana4/xml/app.js
@@ -1,78 +1,94 @@
-// ejercicios con arrays
 
-// pasos ejercicio con XML:
-// generar el array desde el XML
-// AÑADIR un botón para cada orden:
-// 1 - ascendente por nombre de usuario
-// 2 - asecendente por clave de usuario
-// 3 - mostrar solamente el usuario solicitado
-//    (input - .slice .indexOf)
 
-// variable global de la pagina
-// un array con un elemento por cada usuario del XML
-// en realidad será un array de arrays, una matriz bidimensional
-let registrados = [];
+function direccionDeOrden(dir){
+    if(dir == "mayor"){
+        return 0;
+    }else if(dir == "menor"){
+        return 1;
+    }else{
+        document.write('Por favor, introduce "mayor" o "menor"');
+    }
+  
+}
 
+
+    
+    let arrayDesordenado = [6]
+    for (let i = 0; i < 6; i++) {
+        let num = Math.floor(Math.random() * 7) + 1;
+        arrayDesordenado[i] = num;
+    }
+    var devolucion = document.getElementById('arrayDesordenado');
+    devolucion.innerHTML = arrayDesordenado;
+
+    function ordenar(mayor){
+        arrayDesordenado.sort(function(a, b) {
+            return b - a;
+          });
+        if(mayor == 1){
+              devolucion.innerHTML = arrayDesordenado;
+        }else{
+            devolucion.innerHTML = arrayDesordenado.reverse();
+        }
+        
+        
+    }
+
+    let registrados = [];
+leerXML();
 
 function leerXML() {
     // lee desde GitHub.
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            cargarArray(this);
-        }
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        miFuncion(this);
+      }
     };
-    //xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados2.xml", true);
     xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados2.xml", true);
     xhr.send();
-}
+  }
 
-function cargarArray(xml) {
+  function miFuncion(xml) {
     var i;
     var usrNom;
     var usrPsw;
     var usuario = [];
     var xmlDoc = xml.responseXML;
-
+    var tabla="<br/>";
     var x = xmlDoc.getElementsByTagName("usuario");
-    // obtenemos algo así como x=[{USR1},{USR2}...,{CANDIDO}]
+    // obtenemos algo como     x =[{USR1},{USR2},...,{CANDIDO}]
 
-    // tabla es una variable string que contiene codigo
-    // html para poder mostrar en pantalla el XML con formato tabla
-
-    let tabla = "<table><tr><th>EMPLEADO</th><th>CLAVE</th></tr>";
-    for (i = 0; i < x.length; i++) {
-        // leo las etiquetas que me interesan del objeto
-        let usrNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-        let usrPsw = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
-        // actualizo la tabla de visualización
-        tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-        // actualizo el array bidimensional con los usuarios registrados
-        let usuario = [usrNom, usrPsw];
-        registrados.push(usuario);
+    
+    //tabla es una variable String que contiene codigo HTML para poder mostrar en pantalla el XML con formato tabla
+    tabla += "<table><tr><th>Empleado</th><th>Clave</th></tr>";
+    for (i = 0; i <x.length; i++) { 
+      // leo las etiquetas que me interesan del objeto
+      usrNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+      usrPsw = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
+      // actualizo la tabla de visualización
+      tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
+      // actualizo el array bidimensional con los usuarios registrados
+      usuario = [usrNom,usrPsw];
+      registrados.push(usuario);
     }
     tabla += "</table>"
     document.getElementById("mensaje").innerHTML = tabla;
-
+    
     // muestro en consola el array de usuarios registrados
-    // una vez depurado, comentamos el codigo siguiente
-    //   registrados.forEach((usuario) => {
-    //       usuario.forEach((datos) => {
-    //           console.log(datos);
-    //       });
-    //   });
-
-}
+      registrados.forEach((usuario) => {
+          usuario.forEach((datos) => {
+              console.log(datos);
+          });
+      });
 
 
-    // obtenemos el usuario del campo input y
-    // validamos que exista en el array
-    // si no, mostramos un alert y no hacemos nada
-    // si existe actualizamos el elemento con id clavebuscada
+      
+  }
 
-    // sintaxis jQuery: nombre = $("#nom").val();
 
-    function ordenarUsuarios(){
+
+  function ordenarUsuarios(){
         //Ordenamos  la matriz
         registrados.sort((usuario1,usuario2) =>
             usuario1[0].localeCompare(usuario2[0]),
@@ -95,9 +111,7 @@ function cargarArray(xml) {
     document.getElementById("mensaje").innerHTML = tabla;
   }
 
-
-
-function mostrarClaves(){
+  function ordenarClaves(){
     //Ordenamos  la matriz
     registrados.sort((usuario1,usuario2) =>
         usuario2[1].localeCompare(usuario1[1]),
@@ -151,50 +165,3 @@ document.getElementById("mensaje").innerHTML = tabla;
         alert ("Ese usuario no existe, introduce otro");
     }
  }
-
-
-function ascNombre() {
-    // Ordeno la matriz global
-    // acendente por nombre
-    registrados.sort(
-        (usuario1, usuario2) =>
-            usuario1[0].localeCompare(usuario2[0]),
-    );
-    mostrar();
-}
-
-
-
-function desClave() {
-    // Ordeno la matriz global
-    // descendente por clave
-    registrados.sort(
-        (usuario2, usuario1) =>
-            usuario1[1].localeCompare(usuario2[1]),
-    );
-    mostrar();
-}
-
-function mostrar() {
-    // muestro en pantalla el array de usuarios registrados
-    // en formato tabla en el id solicitado
-    let tabla = "";
-    registrados.forEach((usuario) => {
-        usuario.forEach((datos) => {
-            tabla = "<table><tr><th>EMPLEADO</th><th>CLAVE</th></tr>";
-            for (i = 0; i < registrados.length; i++) {
-                // leo las etiquetas que me interesan del objeto
-                usrNom = registrados[i][0];
-                usrPsw = registrados[i][1];
-                // actualizo la tabla de visualización
-                tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-                // actualizo el array bidimensional con los usuarios registrados
-            }
-            tabla += "</table>"
-        });
-    });
-    // la mostramos en el html
-    document.getElementById("solicitado").innerHTML = tabla;
-}
-
-
--- portfolio/Semana4/xml2/app.js
+++ portfolio/Semana4/xml2/app.js
@@ -1,200 +1,133 @@
-// ejercicios con arrays
-
-// pasos ejercicio con XML:
-// generar el array desde el XML
-// AÑADIR un botón para cada orden:
-// 1 - ascendente por nombre de usuario
-// 2 - asecendente por clave de usuario
-// 3 - mostrar solamente el usuario solicitado
-//    (input - .slice .indexOf)
-
-// variable global de la pagina
-// un array con un elemento por cada usuario del XML
-// en realidad será un array de arrays, una matriz bidimensional
+// variable global de la pagina.
+//un array con un elemento por cada usuario (nombre y clave) del xml
+//en realidad sera un array de arrays, una matriz bidimensional
 let registrados = [];
 
-
-function leerXML() {
+function leerXML0() {
     // lee desde GitHub.
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            cargarArray(this);
+            miFuncion(this);
         }
     };
-    
-    xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/datos.xml", true);
+    xhr.open("GET", "datos.xml", true);
+    //xhr.open("GET", "datos.xml", true);
     xhr.send();
 }
 
-function cargarArray(xml) {
+function miFuncion(xml) {
     var i;
-    var usrNom;
-    var usrPsw;
-    var usuario = [];
+    
+    var elemento = [];
     var xmlDoc = xml.responseXML;
-
-    var x = xmlDoc.getElementsByTagName("usuario");
-    // obtenemos algo así como x=[{USR1},{USR2}...,{CANDIDO}]
-
-    // tabla es una variable string que contiene codigo
-    // html para poder mostrar en pantalla el XML con formato tabla
-
-    let tabla = "<table><tr><th>EMPLEADO</th><th>CLAVE</th></tr>";
+    var x = xmlDoc.getElementsByTagName("elemento");
+  
+    let bloque="<div id='carouselExampleIndicators' class='carousel slide' data-ride='carousel'><ol class='carousel-indicators'>"
     for (i = 0; i < x.length; i++) {
+        bloque+="<li data-target='#carouselExampleIndicators' data-slide-to='"+i+"' class='active'></li>"
+    }
+    bloque+="</ol>"
+    bloque+="<div class='carousel-inner'>"
+    let nombre1 = x[0].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+    let url1 = x[0].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
+    let pie1 = x[0].getElementsByTagName("pie")[0].childNodes[0].nodeValue;
+        
+    bloque += "<div class=\"carousel-item active\"><img class=\"d-block w-100\" src='" + url1 + 
+    "'alt=\"First slide\"'/><div class='carousel-caption d-none d-md-block'><h5>"+nombre1+"</h5><p>"+pie1+"</p></div></div>";
+    
+    for (i = 1; i < x.length; i++) {
         // leo las etiquetas que me interesan del objeto
-        let usrNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-        let usrPsw = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
+        let nombre = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+        let url = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
+        let pie = x[i].getElementsByTagName("pie")[0].childNodes[0].nodeValue;
         // actualizo la tabla de visualización
-        tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
+        
+               
+            bloque += "<div class=\"carousel-item \"><img class=\"d-block w-100\" src='" + url + 
+                "'alt=\"First slide\"'/><div class='carousel-caption d-none d-md-block'><h5>"+nombre+"</h5><p>"+pie+"</p></div></div>";
+        
+    
+        elemento = [nombre, url];
+        registrados.push(elemento);
+    }
+                bloque += "</div>"
+                bloque+="<a class='carousel-control-prev' href='#carouselExampleIndicators' role='button' data-slide='prev'><span class='carousel-control-prev-icon' aria-hidden='true'></span><span class='sr-only'>Previous</span></a>"
+                bloque+="<a class='carousel-control-next' href='#carouselExampleIndicators' role='button' data-slide='next'><span class='carousel-control-next-icon' aria-hidden='true'></span><span class='sr-only'>Next</span></a>"
         // actualizo el array bidimensional con los usuarios registrados
-        let usuario = [usrNom, usrPsw];
-        registrados.push(usuario);
-    }
-    tabla += "</table>"
-    document.getElementById("mensaje").innerHTML = tabla;
+        
 
-    // muestro en consola el array de usuarios registrados
-    // una vez depurado, comentamos el codigo siguiente
-    //   registrados.forEach((usuario) => {
-    //       usuario.forEach((datos) => {
-    //           console.log(datos);
-    //       });
-    //   });
+    
+    
+    bloque += "</div>"
+    document.getElementById("mensaje").innerHTML = bloque;
 
-}
+    // muestro en consola el array de usuarios registrados, pero no es necesario
+    registrados.forEach((orla) => {
+        orla.forEach((datos) => {
+            console.log(datos);
 
-
-    // obtenemos el usuario del campo input y
-    // validamos que exista en el array
-    // si no, mostramos un alert y no hacemos nada
-    // si existe actualizamos el elemento con id clavebuscada
-
-    // sintaxis jQuery: nombre = $("#nom").val();
-
-    function ordenarUsuarios(){
-        //Ordenamos  la matriz
-        registrados.sort((usuario1,usuario2) =>
-            usuario1[0].localeCompare(usuario2[0]),
-        );
-        let tabla="";
-    registrados.forEach((usuario) => {
-      usuario.forEach((datos) => {
-        tabla = "<table><tr><th>Empleado</th><th>Clave</th></tr>";
-        for (let i = 0; i <registrados.length; i++) { 
-          // leo las etiquetas que me interesan del objeto
-          let usrNom = registrados[i][0];
-          let usrPsw = registrados[i][1];
-            // actualizo la tabla de visualización
-          tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-          // actualizo el array bidimensional con los usuarios registrados
-        }
-        tabla += "</table>"
-      });
-    });
-    document.getElementById("mensaje").innerHTML = tabla;
-  }
-
-
-
-function mostrarClaves(){
-    //Ordenamos  la matriz
-    registrados.sort((usuario1,usuario2) =>
-        usuario2[1].localeCompare(usuario1[1]),
-    );
-    let tabla="";
-    registrados.forEach((usuario) => {
-    usuario.forEach((datos) => {
-    tabla = "<table><tr><th>Empleado</th><th>Clave</th></tr>";
-    for (let i = 0; i <registrados.length; i++) { 
-      // leo las etiquetas que me interesan del objeto
-      let usrNom = registrados[i][0];
-      let usrPsw = registrados[i][1];
-        // actualizo la tabla de visualización
-      tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-      // actualizo el array bidimensional con los usuarios registrados
-    }
-    tabla += "</table>"
-  });
-});
-document.getElementById("mensaje").innerHTML = tabla;
-}
-
- function filtrarBusqueda(){
-     let name = document.getElementById('nombre').value;
-    let cont = 0;
-    let tabla="";
-    let salida = false;
-    registrados.forEach((usuario) => {
-    usuario.forEach((datos) => {
-        if(!salida){
-    tabla = "<table><tr><th>Empleado</th><th>Clave</th></tr>";
-    for (let i = 0; i <registrados.length; i++) { 
-
-      let usrNom = registrados[i][0];
-      let usrPsw = registrados[i][1];
-        // actualizo la tabla de visualización
-        if(usrNom == name){
-            tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-        }else{
-            salida = true;
-            cont++;
-        }
-    }
-    tabla += "</table>"
-}
-  });
-});
-    if(cont < registrados.length){
-    document.getElementById("mensaje").innerHTML = tabla;
-    }else{
-        alert ("Ese usuario no existe, introduce otro");
-    }
- }
-
-
-function ascNombre() {
-    // Ordeno la matriz global
-    // acendente por nombre
-    registrados.sort(
-        (usuario1, usuario2) =>
-            usuario1[0].localeCompare(usuario2[0]),
-    );
-    mostrar();
-}
-
-
-
-function desClave() {
-    // Ordeno la matriz global
-    // descendente por clave
-    registrados.sort(
-        (usuario2, usuario1) =>
-            usuario1[1].localeCompare(usuario2[1]),
-    );
-    mostrar();
-}
-
-function mostrar() {
-    // muestro en pantalla el array de usuarios registrados
-    // en formato tabla en el id solicitado
-    let tabla = "";
-    registrados.forEach((usuario) => {
-        usuario.forEach((datos) => {
-            tabla = "<table><tr><th>EMPLEADO</th><th>CLAVE</th></tr>";
-            for (i = 0; i < registrados.length; i++) {
-                // leo las etiquetas que me interesan del objeto
-                usrNom = registrados[i][0];
-                usrPsw = registrados[i][1];
-                // actualizo la tabla de visualización
-                tabla += "<tr><td>" + usrNom + "</td><td>" + usrPsw + "</td></tr>";
-                // actualizo el array bidimensional con los usuarios registrados
-            }
-            tabla += "</table>"
         });
     });
-    // la mostramos en el html
-    document.getElementById("solicitado").innerHTML = tabla;
 }
 
 
+function MayoramenorUsuario() {
+    let nombres = [];
+
+    for (let i = 0; i < registrados.length; i++) {
+        registrados.sort();
+        nombres[i] = registrados[i][0];
+    }
+    document.getElementById("mensaje2").innerHTML = (`El array de mayor a menor es: ` + nombres);
+    //document.write("El array de mayor a menor es: "+ nombres);      
+}
+function MenoramayorUsuario() {
+    let nombres = [];
+
+    for (let i = 0; i < registrados.length; i++) {
+        registrados.sort();
+        registrados.reverse();
+        nombres[i] = registrados[i][0];
+    }
+    document.getElementById("mensaje2").innerHTML = (`El array de mayor a menor es: ` + nombres);
+    //document.write("El array de mayor a menor es: "+ nombres);      
+}
+let claves = [];
+function MayoramenorClave() {
+    let claves = [];
+
+    for (let i = 0; i < registrados.length; i++) {
+        registrados.sort();
+        claves[i] = registrados[i][1];
+    }
+    document.getElementById("mensaje2").innerHTML = (`El array de mayor a menor es: ` + claves);
+    //document.write("El array de mayor a menor es: "+ nombres);      
+}
+function MenoramayorClave() {
+    let claves = [];
+
+    for (let i = 0; i < registrados.length; i++) {
+        registrados.sort();
+        registrados.reverse();
+        claves[i] = registrados[i][1];
+    }
+    document.getElementById("mensaje2").innerHTML = (`El array de mayor a menor es: ` + claves);
+    //document.write("El array de mayor a menor es: "+ nombres);      
+}
+
+function mostrarClave() {
+    let nombre = $("#nom").val();
+    let url=null;
+  
+    for (let i = 0; i < registrados.length; i++) {
+        if (nombre == registrados[i][0]) {
+            url=registrados[i][1]           
+        }
+    }
+    if (url==null){
+        alert(`Ese mobre de usuario no existe. Por favor intentelo de neuvo`);
+    }
+    document.getElementById("claveSolicidada").innerHTML = (`La url de ` + nombre + ` es ` + url);
+
+}
